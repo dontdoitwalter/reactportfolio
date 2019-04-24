6840fae5adb21ba5fa4a7a5e883e5bd385d5ba28
Walter Van Hooser <emailwaltervanhooser@gmail.com>
2019-04-24 15:03:50 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Header from './components/header/Header';
 import Splash from './components/splash/Splash';
-import Footer from './components/footer/Footer';
 
 
 export default class App extends Component {
@@ -12,7 +11,6 @@ export default class App extends Component {
       <div className="App">
         <Header />
         <Splash />
-        <Footer />
       </div>
   );
   }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './header.css';
+import {Container, Row, Col} from 'reactstrap';
+import {LOGO} from '../../components/assets/laptop.svg';
 
 const Header = () =>{
     return(
-        <div id='header'>
-            HEADER
-        </div>
+        <Container id='header'>
+            <Row>
+                <Col id='logo-col'>
+                    <img src={LOGO} alt='logo' />
+                </Col>
+                <Col id='nav-col'>
+                    <p>Home Projects Social Contact</p>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 export default Header;
